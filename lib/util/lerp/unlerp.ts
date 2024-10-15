ff77ebce58b1c74b24874db9adebe263277d8c17
dupasj <jeremiedupas@yahoo.fr>
2024-10-15 18:11:28 +0200

    Fix unlerp function when the the max < min & clamp is true

--- lib/util/lerp/unlerp.ts
+++ lib/util/lerp/unlerp.ts
@@ -1,5 +1,9 @@
 const unlerp = (min: number,max: number,value: number,clamp = true): number => {
-  const clamped = clamp ? Math.min(max, Math.max(min, value)) : value;
+
+  const __min = Math.min(min,max);
+  const __max = Math.max(min,max);
+
+  const clamped = clamp ? Math.min(__max, Math.max(__min, value)) : value;
   return (clamped - min) / (max - min);
 };
 
