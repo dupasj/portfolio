1807205717bbcec6a5cb391134bca6c971c064af
dupasj <dupasj97@gmail.com>
2024-10-16 11:56:02 +0200

    Make all the article as a link in the list of articles

--- components/SliceListOf.vue
+++ components/SliceListOf.vue
@@ -8,14 +8,14 @@
     </div>
 
     <div class="mt-[30px] xl:mt-[90px] grid grid-cols-2 xl:grid-cols-3 gap-x-[15px] md:gap-x-[30px] gap-y-[44px]">
-      <article class="max-w-[370px]" v-for="item in items" :key="item.id">
+      <NuxtLink  :to="{ name: `${type}-uid___${locale}`, params: { uid: item.uid } }" class="max-w-[370px]" v-for="item in items" :key="item.id">
         <img :src="item.data.preview.url" :alt="item.data.preview.alt" class="scroll-animation object-cover w-full h-auto aspect-[4/3] rounded-[5px]"/>
         <h4 class="scroll-animation mb-[10px] mt-[10px] block w-full h-auto text-white text-[20px] md:text-[30px] font-medium m-0 p-0 font-sans subpixel-antialiased">{{ $prismic.asText(item.data.title) }}</h4>
         <p class="scroll-animation mt-[6px] block w-full h-auto max-h-[60px] text-clip overflow-hidden text-white text-[13px] md:text-[16px] font-medium m-0 p-0 font-sans subpixel-antialiased leading-[20px]">{{ $prismic.asText(item.data.ellipsis) }}</p>
-        <NuxtLink class="scroll-animation mt-[5px] md:mt-[7px] block w-full h-auto text-white text-[14px] md:text-[16px] font-medium m-0 p-0 font-sans subpixel-antialiased" :to="{ name: `${type}-uid___${locale}`, params: { uid: item.uid } }">
+        <button class="inline-block scroll-animation mt-[5px] md:mt-[7px] w-auto h-auto text-white text-[14px] md:text-[16px] font-medium m-0 p-0 font-sans subpixel-antialiased">
           {{$t('more')}}
-        </NuxtLink>
-      </article>
+        </button>
+      </NuxtLink>
     </div>
   </Container>
 </template>
