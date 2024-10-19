b1f4a22852245b3dff67d553b92ba989b0492452
dupasj <dupasj97@gmail.com>
2024-10-19 14:57:37 +0200

    Delete transform hit fix

--- components/SliceContactMe.vue
+++ components/SliceContactMe.vue
@@ -9,8 +9,8 @@
             <svg v-if="item.type === 'github'" xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="m21.62 11.108-8.731-8.729a1.292 1.292 0 0 0-1.823 0L9.257 4.19l2.299 2.3a1.532 1.532 0 0 1 1.939 1.95l2.214 2.217a1.53 1.53 0 0 1 1.583 2.531c-.599.6-1.566.6-2.166 0a1.536 1.536 0 0 1-.337-1.662l-2.074-2.063V14.9c.146.071.286.169.407.29a1.537 1.537 0 0 1 0 2.166 1.536 1.536 0 0 1-2.174 0 1.528 1.528 0 0 1 0-2.164c.152-.15.322-.264.504-.339v-5.49a1.529 1.529 0 0 1-.83-2.008l-2.26-2.271-5.987 5.982c-.5.504-.5 1.32 0 1.824l8.731 8.729a1.286 1.286 0 0 0 1.821 0l8.69-8.689a1.284 1.284 0 0 0 .003-1.822"></path></svg>
             <svg v-if="item.type === 'file'" xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M19 9h-4V3H9v6H5l7 8zM4 19h16v2H4z"></path></svg>
           </div>
-          <p class="scroll-animation no-underline w-full h-auto xl:text-[20px] text-[16px] text-white xl:leading-[27px] leading-[20px] m-0 p-0">{{ transform(item.title)  }}</p>
-          <span class="scroll-animation no-underline w-full h-auto xl:text-[30px] text-[20px] text-white xl:leading-[40px] leading-[30px] m-0 p-0">{{ transform(item.action_text)  }}</span>
+          <p class="scroll-animation no-underline w-full h-auto xl:text-[20px] text-[16px] text-white xl:leading-[27px] leading-[20px] m-0 p-0">{{ item.title  }}</p>
+          <span class="scroll-animation no-underline w-full h-auto xl:text-[30px] text-[20px] text-white xl:leading-[40px] leading-[30px] m-0 p-0">{{ item.action_text  }}</span>
         </a>
       </div>
         <div class="right">
@@ -49,16 +49,6 @@ const active = ref(false)
 const nameEL = ref(null)
 const emailEL = ref(null)
 const contentEL = ref(null)
-
-const transform = (input) => {
-  const raw = app.$prismic.asText(input);
-  if (raw.toLowerCase().trim().endsWith("mon cv") && app.$i18n.locale === "en"){
-    return "Download my CV";
-  }
-
-
-  return raw;
-}
 
 const updateForm = () => {
   active.value = (nameEL.value.value.trim().length > 0 && emailEL.value.value.trim().length > 0 && contentEL.value.value.trim().length > 0);
