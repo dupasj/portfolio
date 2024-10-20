eb7bed16b57d0cf8f07daf1cb7a9bab5bd68fa97
dupasj <dupasj97@gmail.com>
2024-10-20 15:58:54 +0200

    Delete file url

--- components/SliceContactMe.vue
+++ components/SliceContactMe.vue
@@ -2,7 +2,7 @@
   <Container :section="true" :padding="false">
     <div class="blur-inner-container relative w-full block xl:grid xl:grid-cols-2 rounded-[0px] xl:rounded-[30px]">
       <div class="left space-y-[30px]">
-        <a :href="item.target_link.url ?? item.file.url ?? null" v-for="item of props.slice.items" class="w-full relative xl:pl-[76px] pl-[60px] no-underline">
+        <a download :href="item.target_link.url ?? null" v-for="item of props.slice.items" class="w-full relative xl:pl-[76px] pl-[60px] no-underline">
           <div class="scroll-animation absolute left-0 top-[50%] -translate-y-1/2 xl:w-[56px] xl:h-[56px] w-[45px] h-[45px] rounded-full border border-[#52486C] flex justify-center items-center content-center justify-items-center">
             <svg v-if="item.type === 'phone'" xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgba(255, 255, 255, 1);msFilter:progid:DXImageTransform.Microsoft.BasicImage(rotation=0, mirror=1);"><path d="m20.487 17.14-4.065-3.696a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a.997.997 0 0 0-.085-1.39z"></path></svg>
             <svg v-if="item.type === 'email'" xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 4.7-8 5.334L4 8.7V6.297l8 5.333 8-5.333V8.7z"></path></svg>
