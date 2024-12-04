8365483a14050785e268053cf58f7533b3c74edd
dupasj <dupasj97@gmail.com>
2024-12-04 16:01:23 +0100

    Force props.screen boolean value

--- components/Container.vue
+++ components/Container.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="w-full h-auto" :class="{
       'mt-[100px] xl:mb-[100px] [&:not(:last-child)]:mb-[100px]': props.margin,
-      'fill-with-margin pt-[160px]': props.screen && !props.section && !props.container && props.margin,
-      'pt-[160px]': props.screen && !props.section && !props.container,
-      'snap': props.section || props.screen,
-      'min-h-[100vh]': props.screen && !props.section && !props.container && !props.margin,
+      'fill-with-margin pt-[160px]': props.screen === true && !props.section && !props.container && props.margin,
+      'pt-[160px]': props.screen === true && !props.section && !props.container,
+      'snap': props.section || props.screen === true,
+      'min-h-[100vh]': props.screen === true && !props.section && !props.container && !props.margin,
       'pl-[15px] pr-[15px]': props.padding && !props.container
   }">
     <div :class="{
-      'fill-with-margin': props.screen && props.margin,
-      'pt-[160px]': props.screen,
-      'min-h-[100vh]': props.screen && !props.margin,
+      'fill-with-margin': props.screen === true && props.margin,
+      'pt-[160px]': props.screen === true,
+      'min-h-[100vh]': props.screen === true && !props.margin,
       'pl-[15px] pr-[15px]': props.padding
     }" v-if="props.section" class="xl:container flex-col flex justify-items-center items-center justify-center align-center content-center">
       <slot/>
     </div>
     <div :class="{
-      'fill-with-margin': props.screen && props.margin,
-      'pt-[160px]': props.screen,
-      'min-h-[100vh]': props.screen && !props.margin,
+      'fill-with-margin': props.screen === true && props.margin,
+      'pt-[160px]': props.screen === true,
+      'min-h-[100vh]': props.screen === true && !props.margin,
       'pl-[15px] pr-[15px]': props.padding
     }" v-else-if="props.container" class="container flex-col flex justify-items-center items-center justify-center align-center content-center">
       <slot/>
