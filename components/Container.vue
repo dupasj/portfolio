f8e5c65a3670dc31fe53502619c1c531af74f703
dupasj <dupasj97@gmail.com>
2024-12-04 16:04:40 +0100

    Duplicate min-height check for mobile only

--- components/Container.vue
+++ components/Container.vue
@@ -2,23 +2,32 @@
   <div class="w-full h-auto" :class="{
       'mt-[100px] xl:mb-[100px] [&:not(:last-child)]:mb-[100px]': props.margin,
       'fill-with-margin pt-[160px]': props.screen === true && !props.section && !props.container && props.margin,
+      'xl-fill-with-margin xl:pt-[160px]': props.screen === 'mobile' && !props.section && !props.container && props.margin,
       'pt-[160px]': props.screen === true && !props.section && !props.container,
+      'xl:pt-[160px]': props.screen === 'mobile' && !props.section && !props.container,
       'snap': props.section || props.screen === true,
+      'xl:snap': props.section || props.screen === 'mobile',
       'min-h-[100vh]': props.screen === true && !props.section && !props.container && !props.margin,
+      'xl:min-h-[100vh]': props.screen === 'mobile' && !props.section && !props.container && !props.margin,
       'pl-[15px] pr-[15px]': props.padding && !props.container
   }">
     <div :class="{
       'fill-with-margin': props.screen === true && props.margin,
+      'xl-fill-with-margin': props.screen === 'mobile' && props.margin,
       'pt-[160px]': props.screen === true,
+      'xl:pt-[160px]': props.screen === 'mobile',
       'min-h-[100vh]': props.screen === true && !props.margin,
+      'xl:min-h-[100vh]': props.screen === 'mobile' && !props.margin,
       'pl-[15px] pr-[15px]': props.padding
     }" v-if="props.section" class="xl:container flex-col flex justify-items-center items-center justify-center align-center content-center">
       <slot/>
     </div>
     <div :class="{
       'fill-with-margin': props.screen === true && props.margin,
+      'xl-fill-with-margin': props.screen === 'mobile' && props.margin,
       'pt-[160px]': props.screen === true,
       'min-h-[100vh]': props.screen === true && !props.margin,
+      'xl:min-h-[100vh]': props.screen === 'mobile' && !props.margin,
       'pl-[15px] pr-[15px]': props.padding
     }" v-else-if="props.container" class="container flex-col flex justify-items-center items-center justify-center align-center content-center">
       <slot/>
