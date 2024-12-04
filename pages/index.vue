f359d495cb43969abe65779bdd1a4b00e405a528
dupasj <dupasj97@gmail.com>
2024-12-04 17:47:26 +0100

    Update seo homepage seo data #2

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,8 @@ import Background from '../components/Background.vue';
 const { $prismic } = useNuxtApp();
 const { locale } = useI18n();
 const prismic = usePrismic()
+import { useRuntimeConfig, useRoute } from '#imports';
+const config = useRuntimeConfig();
 
 
 const { data, error } = await useAsyncData('homepage', () => prismic.client.getSingle('homepage', {
@@ -35,17 +37,13 @@ watchEffect(() => {
         { hid: 'description', name: 'description', content: 'Portfolio of Jeremie - Homepage' },
         { hid: 'og:title', property: 'og:title', content: 'Portfolio of Jeremie - Homepage' },
         { hid: 'og:description', property: 'og:description', content: 'Portfolio of Jeremie - Homepage' },
-        // { hid: 'og:image', property: 'og:image', content: '/default-image.jpg' },
-        { hid: 'og:url', property: 'og:url', content: window.location.href },
         { hid: 'keywords', name: 'keywords', content: 'portfolio, jeremie, homepage' },
       ],
     });
   } else {
-    // Extract data from Prismic
     const title = $prismic.asText(data.value.data.seo_title);
     const description = $prismic.asText(data.value.data.description);
-    // const image = data.value.data.seo_image?.url || '/fallback-image.jpg'; // Replace with your fallback image URL
-    const keywords = data.value.data.tags || '';
+    const keywords = data.value.data.tags || 'portfolio, jeremie, homepage';
 
     useHead({
       title,
@@ -53,8 +51,6 @@ watchEffect(() => {
         { hid: 'description', name: 'description', content: description },
         { hid: 'og:title', property: 'og:title', content: title },
         { hid: 'og:description', property: 'og:description', content: description },
-        // { hid: 'og:image', property: 'og:image', content: image },
-        { hid: 'og:url', property: 'og:url', content: window.location.href },
         { hid: 'keywords', name: 'keywords', content: keywords },
       ],
     });
